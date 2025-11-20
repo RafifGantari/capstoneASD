5a47d753c583682cd42af2a28ac30a651eb682ff
rafifGantari <rafiftaqiy05@gmail.com>
2025-11-20 09:17:40 +0700

    try

--- src/GraphVisualizer.java
+++ src/GraphVisualizer.java
@@ -104,5 +104,6 @@ public class GraphVisualizer extends JFrame {
             GraphVisualizer visualizer = new GraphVisualizer(graph);
             visualizer.setVisible(true);
         });
+        print("halo dunia")
     }
 }
